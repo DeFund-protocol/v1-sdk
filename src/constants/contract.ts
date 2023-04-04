a9ff9d17236b80a59e89112b9aebab4f46642807
rex <rex.s.bit@gmail.com>
2023-04-04 15:50:14 +0800

    add assert for json file

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -1,8 +1,8 @@
-import { default as ERC20ABI } from '../abis/ERC20.json';
-import { default as FundManagerABI } from '../abis/FundManager.json';
-import { default as PathFinderABI } from '../abis/PathFinder.json';
-import { default as SwapRouter02ABI } from '../abis/SwapRouter02.json';
-import { default as Weth9ABI } from '../abis/Weth9.json';
+import { default as ERC20ABI } from '../abis/ERC20.json' assert { type: 'json' };
+import { default as FundManagerABI } from '../abis/FundManager.json' assert { type: 'json' };
+import { default as PathFinderABI } from '../abis/PathFinder.json' assert { type: 'json' };
+import { default as SwapRouter02ABI } from '../abis/SwapRouter02.json' assert { type: 'json' };
+import { default as Weth9ABI } from '../abis/Weth9.json' assert { type: 'json' };
 
 export {
   FundManagerABI,
