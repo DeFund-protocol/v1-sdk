e3bf3cb118f5ab02f6e4b48a524e77e5fc6d1ec4
rex <rex.s.bit@gmail.com>
2023-04-07 10:21:48 +0800

    add assetsConvert

--- src/composables/useAssetsConvert.ts
+++ src/composables/useAssetsConvert.ts
@@ -30,14 +30,21 @@ export class FundAssetConvert {
     maker: string,
     fundAddress: string,
     params: ConvertParams,
-    overrides?: Overrides) {
+    overrides?: Overrides,
+    refundGas = false
+    ) {
     const convertParams = await this.getConvertParams(fundAddress, params)
+    const executeParams = [
+      fundAddress,
+      convertParams,
+      refundGas,
+    ]
 
     return await SendTransaction(
       this.fundManagerAddress,
       FundManagerABI,
       'sellAssets',
-      convertParams,
+      executeParams,
       overrides,
       this.signer,
     );
@@ -45,8 +52,11 @@ export class FundAssetConvert {
 
   async getConvertParams(fundAddress: string, params: any) {
     let convertParams: any = {
+      sellType: 1,
       fee: 3000,
-      minPriority: 0
+      minPriority: BigNumber.from(0),
+      lastRatio: BigNumber.from(0),
+      underlying: params.tokenOut
     };
 
     const lpAddress = params.lpAddress || constants.AddressZero;
@@ -56,79 +66,74 @@ export class FundAssetConvert {
       true
     );
 
-    if (!params.tokenIn || isEqualAddress(params.tokenIn, constants.AddressZero)) {
-      convertParams['sellType'] = 0;
-      convertParams['lastRatio'] = 10000;
-    } else {
-      convertParams['sellType'] = 1;
-      convertParams['lastRatio'] = 0;
-    }
-
     const assets = [
       ...fundData.tokenBalances.filter((item: any) => !isEqualAddress(item.token, fundData.underlyingToken)),
       ...fundData.lpTokens.map((item: any) => ({ ...item, value: item.amountValue0.add(item.amountValue1) })),
     ].sort((a: any, b: any) => b.priority.sub(a.priority));
-    const targetToken = fundData.tokenBalances.find((item: any) => isEqualAddress(item.token, params.tokenOut));
-    
-    const groupedAssets = [] as Array<any>;
-    assets.forEach((item: any) => {
-      const lastGroup = groupedAssets.length > 0 ? groupedAssets[groupedAssets.length - 1] : undefined;
-      if (lastGroup && lastGroup.length > 0) {
-        const lastItem = lastGroup[0];
-        if (lastItem.priority.eq(item.priority)) {
-          groupedAssets[groupedAssets.length - 1] = [...lastGroup, item];
-        } else {
-          groupedAssets.push([item]);
-        }
-      } else {
-        groupedAssets.push([item]);
-      }
-    });
+    const tokenIn = fundData.tokenBalances.find((item: any) => isEqualAddress(item.token, params.tokenIn));
 
-    let totalAssetAmount = constants.Zero;
-    let assetSellAmount = constants.Zero;
-    if (convertParams['sellType'] === '0') {
-      assetSellAmount = BigNumber.from(fundData.totalValue).mul(BigNumber.from(params.ration)).div(BigNumber.from(100))
-    } else {
-      assetSellAmount = targetToken.mul(BigNumber.from(params.ratio)).div(BigNumber.from(100))
+    if (tokenIn) {
+      convertParams['selectPriority'] = [tokenIn.priority];
+      convertParams['selectRatio'] = [params.ratio];
+
+      const paths = [];
+      for(let i = 0; i < fundData.allowedTokens.length;i++) {
+
+        if(isEqualAddress(fundData.allowedTokens[i], tokenIn.token)) {
+          const trade = await exactInputPath(params.tokenIn, params.tokenOut, tokenIn.balance.mul(BigNumber.from(params.ratio)).div(BigNumber.from(1e4)), this.chainId, this.signer);
+          paths.push(trade.path)
+        } else {
+          paths.push('0x')
+        }
+      }
+      convertParams['paths'] = paths;
+
+      return convertParams;
     }
+
+    const needValue = fundData.totalValue.mul(BigNumber.from(params.ratio)).div(BigNumber.from(1e4));
+    const selectPriority = [];
+    const selectRatio = [];
+    let selectedValue = constants.Zero;
+    for (let i = 0; i < assets.length; i++) {
+      selectPriority.push(assets[i].priority)
+      if (selectedValue.gt(needValue)) {
+        const leftValue = needValue.sub(selectedValue);
+        const ratio = leftValue.mul(1000).div(assets[i].value)
+        selectRatio.push(ratio)
+        break;
+      } else {
+        selectRatio.push(1e4);
+        selectedValue = selectedValue.add(assets[i].value)
+      }
+    }
+
+    convertParams['selectPriority'] = selectPriority;
+    convertParams['selectRatio'] = selectRatio;
+
+
 
     const balances = {} as any;
 
-    for (const group of groupedAssets) {
-      const value = group.reduce((acc: BigNumber, cur: any) => {
-        return acc.add(cur.value);
-      }, constants.Zero);
 
-      if (totalAssetAmount.add(value).gte(assetSellAmount)) {
-        const lastAmount = assetSellAmount.sub(totalAssetAmount);
-        if (params.ratio !== 100) params.lastRatio = lastAmount.mul(10000).div(value);
-        params.minPriority = group[0].priority;
+    for (const asset of assets) {
+      if (asset.token && asset.balance.eq(constants.Zero)) continue;
+      if (asset.tokenId && asset.amount0.add(asset.fee0).eq(constants.Zero) && asset.amount1.add(asset.fee1).eq(constants.Zero))
+        continue;
+
+      if (asset.token) {
+        const amount = asset.balance.mul(params.lastRatio).div(BigNumber.from(1e4));
+        balances[asset.token] = balances[asset.token] ? balances[asset.token].add(amount) : amount;
       } else {
-        totalAssetAmount = totalAssetAmount.add(value);
+        const amount0 = asset.amount0.mul(params.lastRatio).div(BigNumber.from(1e4));
+        const amount1 = asset.amount1.mul(params.lastRatio).div(BigNumber.from(1e4));
+        balances[asset.token0] = (balances[asset.token0] ? balances[asset.token0].add(amount0) : amount0).add(
+          asset.fee0
+        );
+        balances[asset.token1] = (balances[asset.token1] ? balances[asset.token1].add(amount1) : amount1).add(
+          asset.fee1
+        );
       }
-
-      for (const asset of group) {
-        if (asset.token && asset.balance.eq(constants.Zero)) continue;
-        if (asset.tokenId && asset.amount0.add(asset.fee0).eq(constants.Zero) && asset.amount1.add(asset.fee1).eq(constants.Zero))
-          continue;
-
-        if (asset.token) {
-          const amount = asset.balance.mul(params.lastRatio).div(10000);
-          balances[asset.token] = balances[asset.token] ? balances[asset.token].add(amount) : amount;
-        } else {
-          const amount0 = asset.amount0.mul(params.lastRatio).div(10000);
-          const amount1 = asset.amount1.mul(params.lastRatio).div(10000);
-          balances[asset.token0] = (balances[asset.token0] ? balances[asset.token0].add(amount0) : amount0).add(
-            asset.fee0
-          );
-          balances[asset.token1] = (balances[asset.token1] ? balances[asset.token1].add(amount1) : amount1).add(
-            asset.fee1
-          );
-        }
-      }
-
-      if (params.minPriority.gt(constants.Zero)) break;
     }
 
     const paths = [];
@@ -146,7 +151,6 @@ export class FundAssetConvert {
       }
     }
     convertParams['paths'] = paths;
-
     return convertParams;
   }
 }
